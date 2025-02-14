8d95aba4418e801dd84e08cd3c0dbfd5bc44f12f
MarcosPc <maagustin51@gmail.com>
2025-02-14 15:34:17 -0300

    comenzando la configuracon de jwt

---
+++ src/main/java/com/sipAndBites/controller/ControllerAuth.java
@@ -0,0 +1,13 @@
+package com.sipAndBites.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class ControllerAuth {
+
+
+}
